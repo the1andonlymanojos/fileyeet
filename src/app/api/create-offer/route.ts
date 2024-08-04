e8d15079f302acbfcc5da73ec558477e808fc0ed
the1andonlymanojos <mshivagange@gmail.com>
2024-08-04 18:28:06 +0530

    added cloud db

--- src/app/api/create-offer/route.ts
+++ src/app/api/create-offer/route.ts
@@ -1,10 +1,14 @@
 // app/api/create-offer/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
+//import { v4 as uuidv4 } from 'uuid';
 import { createCall } from '@/lib/store';
 
 export async function POST(req: NextRequest) {
-    const callId = uuidv4();
+    const generateRandomCallId = () => {
+        return Math.floor(100000 + Math.random() * 900000).toString();
+    };
+
+    const callId = generateRandomCallId();
     const { offer } = await req.json();
 
     await createCall(callId, offer);
