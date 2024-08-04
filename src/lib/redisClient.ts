9e1469446884c4e61059e9577c504a01d13c3ff6
the1andonlymanojos <mshivagange@gmail.com>
2024-08-04 18:22:00 +0530

    added cloud db

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,7 +1,10 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(
-
-);
+const redis = new  Redis({
+    port: 16874, // Redis port
+    host: "redis-16874.c93.us-east-1-3.ec2.redns.redis-cloud.com", // Redis host
+    username: "default", // needs Redis >= 6
+    password: process.env.REDIS_PASSWORD,
+});
 
 export default redis;
