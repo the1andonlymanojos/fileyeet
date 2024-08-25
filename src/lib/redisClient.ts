0f76afcd4afd0f44f2be5eb33defe47e5f3aa90b
the1andonlymanojos <mshivagange@gmail.com>
2024-08-25 15:13:58 +0530

    added logs

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,11 +1,17 @@
 import Redis from 'ioredis';
+import {red} from "next/dist/lib/picocolors";
+let redis;
+if (process.env.REDIS_PASSWORD){
+ redis = new  Redis({
+  port: 16874, // Redis port
+  host: "redis-16874.c93.us-east-1-3.ec2.redns.redis-cloud.com", // Redis host
+  username: "default", // needs Redis >= 6
+  password: 'process.env.REDIS_PASSWORD',
+ });
+} else {
+ redis = new Redis({});
+}
 
-const redis = new  Redis({
-    port: 16874, // Redis port
-    host: "redis-16874.c93.us-east-1-3.ec2.redns.redis-cloud.com", // Redis host
-    username: "default", // needs Redis >= 6
-    password: 'XV8hqWunqnLEBOPXKJ15nwt63zGVd301',
-});
 
 
 //const redis = new Redis()
