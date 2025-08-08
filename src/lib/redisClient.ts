ee8ac5c4372274a276d2e6ed4f82b7221e3d53c6
Manoj Shiv <115482304+the1andonlymanojos@users.noreply.github.com>
2025-08-08 20:10:36 +0530

    Update redisClient.ts

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -3,8 +3,8 @@ import { Redis } from 'ioredis';
 let redis:Redis;
 if (process.env.REDIS_PASSWORD){
  redis = new  Redis({
-  port: 18764, // Redis port
-  host: "redis-18764.crce179.ap-south-1-1.ec2.redns.redis-cloud.com", // Redis host
+  port: 12036, // Redis port
+  host: "redis-12036.crce206.ap-south-1-1.ec2.redns.redis-cloud.com", // Redis host
   username: "default", // needs Redis >= 6
   password: process.env.REDIS_PASSWORD,
  });
